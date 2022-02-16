f9fa364863e11db4d15b6615462de373457f744f
codeXpawan <shahpawan56@gmail.com>
2022-02-16 14:40:41 +0545

    labsheet-4 question 5 added

---
+++ labsheet-4/question-5.cpp
@@ -0,0 +1,35 @@
+/* Write a program that illustrates the following relationship and comment on them. 
+
+i) const_object.non_const_mem_function
+
+ii) const_object.const_mem_function
+
+iii) non_const_object.non_const_mem_function
+
+iv) non_const_object.const_mem_function
+*/
+#include<iostream>
+using namespace std;
+
+class constant{
+	int i;
+	public:
+	constant(int x=0){
+		i = x;
+	}
+	int display(){
+		return i;
+	}
+	int getVal()const{
+		return i;
+	}
+};
+int main(){
+	constant c(8);
+	const constant c1(18);
+	//cout<<"\nCalling non-constant member function from constant object. Val = "<<c1.display();  // Error occured if uncomment because constant object can only call constant member function
+	cout<<"\nCalling constant member function from constant object. Val = "<<c1.getVal(); //Constant object can call constant member function
+	cout<<"\nCalling non-cconstant member function from non-constant object. Val = "<<c.display(); //non-constant object can call non-constant member function as well as constant member function
+	cout<<"\nCalling constant member function from non-constant object. Val = "<<c.getVal(); //non-constant object can call non-constant member function as well as constant member function
+	return 0;
+}

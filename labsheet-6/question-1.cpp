d4a7226b7a74c4bdbd6b8aeb9259a3acf52f9923
codeXpawan <shahpawan56@gmail.com>
2022-03-08 09:59:43 +0545

    labsheet 6 question 1 modified

--- labsheet-6/question-1.cpp
+++ labsheet-6/question-1.cpp
@@ -6,19 +6,38 @@ class dist{
 	float m, cm;
 	public:
 	dist():m(0),cm(0){}
-	dist(float x, float y):m(x),cm(y){}
-	dist convert(){
-		return dist(m*100,cm/100);
+	//dist(float x, float y):m(x),cm(y){}
+	void convert_m(int x, int y){
+		m = static_cast<float>(x)+static_cast<float>(y)/100;
 	}
-	void display(){
+	void convert_cm(int x, int y){
+		cm = static_cast<float>(x)*100+static_cast<float>(y);
+	}
+	void diaplay(){
 		cout<<m<<" and "<<cm;
 	}
-
 };
 int main(){
-	dist d(1,100),ans;
-	ans = d.convert();
+	dist d;
+	int ans,m,cm;
+	cout<<"1.Convert the values to meter.\n2.Convert the valus to cm.";
+	cout<<"The number you want to perform task: ";
+	cin>>ans;
+	if(ans == 1){
+		cout<<"Enter the value of meter: ";
+		cin>>m;
+		cout<<"Enter the value of centimeter: ";
+		cin>>cm;
+		d.convert_m(m,cm);
+	}
+	else if(ans == 2){
+		cout<<"Enter the value of meter: ";
+		cin>>m;
+		cout<<"Enter the value of centimeter: ";
+		cin>>cm;
+		d.convert_cm(m,cm);
+	}
 	cout<<"The converted value to centimeter and meter is ";
-	ans.display();
+	d.diaplay();
 	return 0;
 }
